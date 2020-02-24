5c867530223b3f68161b07d1aff2306f33529db0
Nobuaki.Mitsuma <nonobu722@gmail.com>
2020-02-24 18:10:05 +0900

    ユーザーと投稿を関連付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :authentication_keys => [:name]
 
+  has_many :posts
+
   def email_required?
     false
   end
