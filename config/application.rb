ed5a223cbb0b742c1f351f21d139f2f249783724
Nobuaki.Mitsuma <nonobu722@gmail.com>
2020-02-24 15:51:04 +0900

    ユーザー登録/ログイン機能を実装

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module MiniBlog1
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     # デフォルトのlocaleを日本語(:ja)にする
-    config.i18n.default_locale = :ja
+    # config.i18n.default_locale = :ja
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
